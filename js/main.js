23664e6e4401fa238e12843d46062e7643d73f46
Sujit Barua <cse.sujitbarua@gmail.com>
2017-04-22 03:07:14 +0600

    This is new site designig

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".product-list").masonry();
+       
+    
+    });
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
